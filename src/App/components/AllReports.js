1628e63678f6c8e599a6f35fbaee5488289975d1
Hari <hari.harmalkar@gmail.com>
2020-01-18 12:53:42 +0530

    drop down added for report filtering

--- src/App/components/AllReports.js
+++ src/App/components/AllReports.js
@@ -6,7 +6,8 @@ import {
   Form,
   Button,
   InputGroup,
-  Table
+  Table,
+  Dropdown
 } from "react-bootstrap";
 
 import Aux from "../../hoc/_Aux";
@@ -20,7 +21,9 @@ class AllReports extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      areadId: parseInt(getQueryParamValue(props, QUERY_PARAM_AREA_ID), 10)
+      areadId: parseInt(getQueryParamValue(props, QUERY_PARAM_AREA_ID), 10),
+      userFilterId: null,
+      userName: "Select Manager"
     };
   }
 
@@ -28,7 +31,7 @@ class AllReports extends React.Component{
     let prevAreaId = getQueryParamValue(prevProps, QUERY_PARAM_AREA_ID);
     let currentAreaId = getQueryParamValue(this.props, QUERY_PARAM_AREA_ID);
     if (prevAreaId !== currentAreaId) {
-      this.setState({ areadId: parseInt(currentAreaId, 10)});
+      this.setState({ areadId: parseInt(currentAreaId, 10), userName: "Select Manager" });
     }
   }
 
@@ -41,9 +44,46 @@ class AllReports extends React.Component{
     // });
   }
 
-  getListOfReports = () =>{
-    return JSON_LIST_OF_REPORTS.filter(report => report.Area_ID === this.state.areadId);
+  getListOfReports = () => {
+    return JSON_LIST_OF_REPORTS.filter(report => {
+      let userFilter = true;
+      if (this.state.userFilterId) {
+        let user = report.User_Master;
+        if (user) {
+          let userId = user.User_ID;
+          userFilter = userId === this.state.userFilterId;
+        } else {
+          userFilter = false;
+        }
+      }
+      return report.Area_ID === this.state.areadId && userFilter
+    });
   }
+
+  getListOfUsers = () => {
+    const userMap = {};
+    const users = [];
+    JSON_LIST_OF_REPORTS.filter(report => report.Area_ID === this.state.areadId).map(report => {
+      let user = report.User_Master;
+      if (user) {
+        let userId = user.User_ID;
+        let userExist = userMap["" + userId + ""];
+        if (!userExist) {
+          userMap["" + userId + ""] = user;
+          users.push(user);
+        }
+      }
+    });
+    return users;
+  }
+
+  onUserFilterChange = (e) => {
+    let userId = e.currentTarget.dataset.userid;
+    let userName = e.currentTarget.dataset.username;
+    this.setState({ userFilterId: userId && userId.length > 0 ? parseInt(e.currentTarget.dataset.userid, 10): null,
+    userName: userName });
+  }
+
 
   // * Render form
   render() {
@@ -55,7 +95,26 @@ class AllReports extends React.Component{
             <Col>
               <Card>
                 <Card.Header>
-                  <Card.Title as="h5">List of reports</Card.Title>
+                  <Row>
+                    <Col xs={8}>
+                      <Card.Title as="h5">List of reports</Card.Title>
+                    </Col>
+                    <Col id="user-filter-drop-down" xs={4}>
+                      <Dropdown alignRight={true}>
+                        <Dropdown.Toggle variant={'link'} id="dropdown-basic">
+                          {this.state.userName}
+                        </Dropdown.Toggle>
+                        <ul>
+                          <Dropdown.Menu>
+                            <li data-userName={"Select Manager"} onClick={this.onUserFilterChange}><a className="dropdown-item">Select Manager</a></li>
+                            {this.getListOfUsers().length > 0 && this.getListOfUsers().map(user => {
+                              return <li data-userName={user.User_Name} data-userId={user.User_ID} onClick={this.onUserFilterChange}><a className="dropdown-item" >{user.User_Name}</a></li>
+                            })}
+                          </Dropdown.Menu>
+                        </ul>
+                      </Dropdown>
+                    </Col>
+                  </Row>
                 </Card.Header>
                 <Card.Body>
                   <Row>
